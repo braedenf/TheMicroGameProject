58351693704f7b48e67a2105afb3042bbbb4422b
Tuurtledove <dschanzel@gmail.com>
2015-09-20 06:30:29 +1200

    Ability to add narrative arcs

---
+++ TheMicroGame/Assets/Scripts/Camera Obscura/Scriptable/Narrative.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+//  -----------------     ----------------     ----------------     ----------------    ----------------     ----------------      // 
+// The "Scriptable Object" Script 
+// - Beneficial For Any Necessiary Additions That May Arise
+
+[System.Serializable]
+public class Narrative : ScriptableObject
+{
+
+public string    creature; 
+public string    state; 
+public string    interaction;
+// ----------  ----------    ----------   ---------- //
+[Multiline] public string    text;
+// ----------  ----------    ----------   ---------- //
+public AudioClip audio;
+
+}
